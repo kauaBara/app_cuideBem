ec39591bd9f26ab9d931762e64af10909d4272a1
Ana <acr291104@gmail.com>
2023-12-08 14:44:56 -0300

    alteração da Ana

--- frontend/App.js
+++ frontend/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>React Native!</Text>
+      <Text>Alteração da Ana!</Text>
       <StatusBar style="auto" />
     </View>
   );
