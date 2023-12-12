9fe9b412556e1ccae5be5ecf700b93893d770ede
Alvaro Henrique <alvaroindaial15@gmail.com>
2023-12-12 20:15:17 -0300

    Criando a branch de desenvolvimento

--- frontend/src/routes/app.routes.js
+++ frontend/src/routes/app.routes.js
@@ -1,11 +1,14 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../screens/Guardian/Home'
-const Stack = createNativeStackNavigator()
 
-export default function AppRoutes(){
-    return(
-        <Stack.Navigator screenOptions={{  headerShown: false}}>
-            <Stack.Screen name='HomeGuardian' component={Home}/>
+// Testes
+export default function AppRoutes() {
+
+    const Stack = createNativeStackNavigator()
+
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name='HomeGuardian' component={Home} />
         </Stack.Navigator>
     )
 }
--- frontend/src/routes/auth.routes.js
+++ frontend/src/routes/auth.routes.js
@@ -1,13 +1,13 @@
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 
 import Identified from '../screens/Identified'
 
-export default function AuthRoutes(){
+export default function AuthRoutes() {
 
     const Stack = createNativeStackNavigator()
 
-    return(
+    return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
             <Stack.Screen name='Identified' component={Identified} />
         </Stack.Navigator>
